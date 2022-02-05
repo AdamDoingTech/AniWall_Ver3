5a090393217679e1679a2b6ecf4f2a5d040c03f1
Adam <olmpy@hotmail.com>
2022-02-04 23:48:08 -0700

    First commit

---
+++ AniWall/AniWallApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniWallApp.swift
+//  AniWall
+//
+//  Created by Adam O. on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct AniWallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AniWall/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  AniWall
+//
+//  Created by Adam O. on 2/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+            GeometryReader { geo in
+                Image("Image3")
+                    .resizable()
+                // Because I want the images to fill the whole screen no matter what the size is.
+                    .scaledToFill()
+                // in order to fill the screen to the edges.
+                    .frame(maxWidth: .infinity)
+                    .frame(maxHeight: .infinity)
+                // The magic code snippit where, the safe ares is ignored in order to fill the screen as a whole.
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: geo.size.width, height: geo.size.height)
+            }
+        }
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+          
+    }
+}
