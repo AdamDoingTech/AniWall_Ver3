0678b2961784ffda2e6018e849ca2608d04ab5e2
Adam <olmpy@hotmail.com>
2022-02-05 01:57:38 -0700

    Changed the orentation of the app

--- AniWall/ContentView.swift
+++ AniWall/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portrait)
           
     }
 }
